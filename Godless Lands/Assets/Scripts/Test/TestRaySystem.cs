6c77ad6d805d9c42f196491762e9e798d8c0f9cc
Viachaslau <vyacheslav.bysel@gmail.com>
2024-01-30 13:15:41 +0100

    - add monster factory - add test terrain system

---
+++ Godless Lands/Assets/Scripts/Test/TestRaySystem.cs
@@ -0,0 +1,63 @@
+﻿using Helpers;
+using Protocol;
+using Protocol.MSG.Game.ToClient;
+using Protocol.MSG.Game.ToServer;
+using RUCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Test
+{
+    internal class TestRaySystem : MonoBehaviour
+    {
+        private NetworkManager _networkManager;
+        private List<Vector3> _points = new List<Vector3>();
+
+        [Inject]
+        public void Construct(NetworkManager networkManager)
+        {
+            _networkManager = networkManager;
+            _networkManager.RegisterHandler(Opcode.MSG_RAYCAST_TEST, OnRaycastTest);
+        }
+
+        private void OnRaycastTest(Packet packet)
+        {
+           packet.Read(out MSG_RAYCAST_TEST_SC msg);
+
+            _points.Clear();
+            Debug.Log($"Raycast result count: {msg.RaycastReslut.Count}");
+            foreach (var point in msg.RaycastReslut)
+            {
+                _points.Add(point.ToUnity());
+            }
+        }
+
+        public void Update()
+        {
+            if (Input.GetButton("Jump"))
+            {
+                MSG_RAYCAST_TEST_CS msg = new MSG_RAYCAST_TEST_CS();
+                _networkManager.Client.Send(msg);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            foreach (var point in _points)
+            {
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _networkManager.UnregisterHandler(Opcode.MSG_RAYCAST_TEST);
+        }
+    }
+}
---
+++ Godless Lands/Assets/Scripts/Walkers/Monsters/MonstersFactory.cs
@@ -0,0 +1,37 @@
+﻿using MonsterRedactor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace Walkers.Monsters
+{
+    public class MonstersFactory : MonoBehaviour
+    {
+        [SerializeField]
+        private MonstersList _monstersList;
+        private DiContainer _diContainer;
+
+        [Inject]
+        private void Construct(DiContainer diContainer)
+        {
+            _diContainer = diContainer;
+        }
+
+        public GameObject CreateMonster(int skinID, Transform transform, Vector3 position)
+        {
+           var monsterData = _monstersList.GetMonster(skinID);
+
+            if(monsterData == null)
+            {
+                Debug.LogError($"Monster with id {skinID} not found");
+                return null;
+            }
+
+            return _diContainer.InstantiatePrefab(monsterData.prefab, position, Quaternion.identity, transform);
+        }
+    }
+}
