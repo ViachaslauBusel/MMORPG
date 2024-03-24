7dda141c2e5f4d1ccb821aa59d67b598f880c7e3
Viachaslau <vyacheslav.bysel@gmail.com>
2024-03-25 00:24:57 +0100

    - Displaying the number of items in the loot drop window

--- Godless Lands/Assets/Scripts/Drop/CellItemDrop.cs
+++ Godless Lands/Assets/Scripts/Drop/CellItemDrop.cs
@@ -7,9 +7,14 @@ using UnityEngine.UI;
 
 public class CellItemDrop : MonoBehaviour {
 
-    public Image item_icon;
-    public Text item_name;
-    public Button item_take;
+    [SerializeField]
+    private Image _item_icon_img;
+    [SerializeField]
+    private Text _item_name_txt;
+    [SerializeField]
+    private Button _item_take_btn;
+    [SerializeField]
+    private Text _countTxt;
     private int item_id;
 
 
@@ -20,14 +25,14 @@ public class CellItemDrop : MonoBehaviour {
 
     public void SetItem(Item _item)
     {
-        item_icon.sprite = Sprite.Create(_item.Data.texture, new Rect(0, 0, _item.Data.texture.width, _item.Data.texture.height), new Vector2(0.5f, 0.5f));
-        item_name.text = _item.Data.nameItem;
+        _item_icon_img.sprite = Sprite.Create(_item.Data.texture, new Rect(0, 0, _item.Data.texture.width, _item.Data.texture.height), new Vector2(0.5f, 0.5f));
+        _item_name_txt.text = _item.Data.nameItem;
+
+        _countTxt.text = _item.Data.stack ? _item.Count.ToString() : "";
 
         item_id = _item.Data.id;
 
-        item_take.onClick.AddListener(() => Take());
-
-        
+        _item_take_btn.onClick.AddListener(() => Take());
     }
 
     public void Take()
--- Godless Lands/Assets/Scripts/Drop/DropWindow.cs
+++ Godless Lands/Assets/Scripts/Drop/DropWindow.cs
@@ -48,7 +48,7 @@ namespace Drop
         {
             GameObject _obj = Instantiate(_cellItemDropPrefab);
             _obj.transform.SetParent(_contentParent);
-            _obj.GetComponent<CellItemDrop>().SetItem(_itemsFactory.CreateItem(item.ItemID));
+            _obj.GetComponent<CellItemDrop>().SetItem(_itemsFactory.CreateItem(item.ItemID, count: item.Count));
             _dropList.Add(_obj);
         }
 
